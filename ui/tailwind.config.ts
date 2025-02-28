2a44dc9436651628bc7d230d04742475f5e45327
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2025-02-27 21:47:58 -0600

    wip: update font family (and serve it locally)

--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        comfortaa: [ "var(--font-comfortaa)" ],
+      }
     },
   },
   plugins: [],
