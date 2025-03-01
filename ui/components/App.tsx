3f2cc656ba8988882fb5180e6b6220097b6a0701
Barrett LaFrance <barrett.t.lafrance@gmail.com>
2025-03-01 13:45:51 -0600

    refactor: push BoardContext init out of App, into Board component

--- ui/components/App.tsx
+++ ui/components/App.tsx
@@ -1,8 +1,6 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { DndContext, DragEndEvent, closestCorners } from '@dnd-kit/core';
-import { arrayMove } from '@dnd-kit/sortable';
 import { Task } from "@/app/api/db";
 import Board, { BoardContext, BoardState, defaultBoardState } from "@/components/Board";
 
@@ -14,31 +12,14 @@ export default function App({tasks}: Readonly<AppProps>) {
   const [boardState, setBoardState] = useState<BoardState>(defaultBoardState);
   useEffect(() => { setBoardState(prev => ({...prev, tasks}) ) }, [tasks]);
 
-  const handleDragEnd = (event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active.id === over?.id) { return ; }
-
-    setBoardState(prev => {
-      const start = boardState.tasks.findIndex(t => t.id === active.id);
-      const final = boardState.tasks.findIndex(t => t.id === over?.id);
-      return {
-        ...prev,
-        tasks: arrayMove(boardState.tasks, start, final),
-      }
-    });
-  }
-
   return (
-      <BoardContext.Provider value={boardState}>
-        <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
-          <Board columns={["New", "WIP", "Done"]} />
+      <BoardContext.Provider value={{boardState, setBoardState}}>
+        <Board columns={["New", "WIP", "Done"]} />
 
-          {/* <div className={`pt-5`}>
-            <h2>Tasks</h2>
-            <pre className={`overflow-auto`}>{JSON.stringify(tasks, null, 2)}</pre>
-          </div> */}
-        </DndContext>
+        {/* <div className={`pt-5`}>
+          <h2>Tasks</h2>
+          <pre className={`overflow-auto`}>{JSON.stringify(tasks, null, 2)}</pre>
+        </div> */}
       </BoardContext.Provider>
   );
 }
--- ui/components/Board.tsx
+++ ui/components/Board.tsx
@@ -1,4 +1,6 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext, Dispatch, SetStateAction, useContext } from 'react';
+import { DndContext, DragEndEvent, closestCorners } from '@dnd-kit/core';
+import { arrayMove } from '@dnd-kit/sortable';
 import Column from "@/components/Column";
 import { Task } from "@/app/api/db";
 
@@ -9,24 +11,50 @@ export const defaultBoardState: BoardState = {
   tasks: [],
 };
 
-export const BoardContext = createContext<BoardState>(defaultBoardState);
+interface SettableBoardContext {
+  boardState: BoardState;
+  setBoardState: Dispatch<SetStateAction<BoardState>>;
+}
+
+export const BoardContext = createContext<SettableBoardContext>({
+  boardState: defaultBoardState,
+  setBoardState: () => {},
+});
 
 interface BoardProps {
   columns: string[];
 }
 
 export default function Board({columns}: Readonly<BoardProps>) {
-  const { tasks } = useContext(BoardContext);
+  const { boardState, setBoardState } = useContext(BoardContext);
+  const { tasks } = boardState;
+
+  const handleDragEnd = (event: DragEndEvent) => {
+    const { active, over } = event;
+
+    if (active.id === over?.id) { return ; }
+
+    setBoardState(prev => {
+      const start = boardState.tasks.findIndex(t => t.id === active.id);
+      const final = boardState.tasks.findIndex(t => t.id === over?.id);
+      return {
+        ...prev,
+        tasks: arrayMove(boardState.tasks, start, final),
+      }
+    });
+  }
 
   return (
-    <div className={`bg-red-300 size-full flex flex-row p-5 gap-5 rounded-lg overflow-auto`}>
-      { columns.map((col, idx) => (
-        <Column
-          title={col}
-          key={col}
-          tasks={tasks.filter(t => t.column === idx)}
-        />
-      ))}
-    </div>
+    <DndContext collisionDetection={closestCorners} onDragEnd={handleDragEnd}>
+      <div className={`bg-red-300 size-full flex flex-row p-5 gap-5 rounded-lg overflow-auto`}>
+        { columns.map((col, idx) => (
+          <Column
+            title={col}
+            key={col}
+            tasks={tasks.filter(t => t.column === idx)}
+          />
+        ))}
+      </div>
+    </DndContext>
   );
 }
